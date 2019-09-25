0dc598d48e004233f82b385131fcff2f9afebe8a
Samuel Gamelin <gamelinsamuel@gmail.com>
2019-09-25 14:50:28 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
+import argparse
 import slack
 
+
+
 def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-t', '--token', type=str, help='The Slack API token.')
     pass
 
 if __name__ == '__main__':
